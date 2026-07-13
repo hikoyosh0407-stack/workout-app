86fde66eac856fb25b48d62f5eb3f20be66c8dca
hikoyosh0407-stack <300842892+hikoyosh0407-stack@users.noreply.github.com>
2026-07-14 06:21:07 +0900

    参考重量を週1実績に更新(ベンチ70/サイド9.5/リア9.5/カール18.5/ワンハンド15)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-v1.0.3';
+const CACHE = 'workout-v1.0.4';
 const ASSETS = ['./', 'index.html', 'plan-data.js', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
