d8b3bf45d30289843836de99d253ab35eb79da9c
hikoyosh0407-stack <300842892+hikoyosh0407-stack@users.noreply.github.com>
2026-07-23 18:31:04 +0900

    ワンハンドダンベルカールの参考重量を10kgに更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-v1.0.6';
+const CACHE = 'workout-v1.0.7';
 const ASSETS = ['./', 'index.html', 'plan-data.js', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
