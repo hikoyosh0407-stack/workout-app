2dcbc7ad7244eb565f3a7702c922b909ccac367c
hikoyosh0407-stack <300842892+hikoyosh0407-stack@users.noreply.github.com>
2026-08-25 06:07:08 +0900

    コーチメモ更新(RDL 1セット目から36kg達成 / ブルガリアン17.5kg伝染警告)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-v1.2.0';
+const CACHE = 'workout-v1.2.1';
 const ASSETS = ['./', 'index.html', 'plan-data.js', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
