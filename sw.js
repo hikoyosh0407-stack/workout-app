cecdb33058327887ae7ab6e3e497048a166d111f
hikoyosh0407-stack <300842892+hikoyosh0407-stack@users.noreply.github.com>
2026-09-05 12:08:43 +0900

    コーチメモ更新(バーチカルプレス最終セット12回達成)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-v1.2.5';
+const CACHE = 'workout-v1.2.6';
 const ASSETS = ['./', 'index.html', 'plan-data.js', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
