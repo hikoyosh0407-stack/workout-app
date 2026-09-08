637c7ed85e6fd58dab9f7462b37ce0c41832c94d
hikoyosh0407-stack <300842892+hikoyosh0407-stack@users.noreply.github.com>
2026-09-09 05:56:09 +0900

    コーチメモ更新(スクワット2回目まだ85kgのまま、増量再警告)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-v1.2.6';
+const CACHE = 'workout-v1.2.7';
 const ASSETS = ['./', 'index.html', 'plan-data.js', 'manifest.webmanifest', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
